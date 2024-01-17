8b4608b1fed0a134ffcef2c5c8bb27e8ad696819
keithvenh <keith.venhuizen@gmail.com>
2024-01-17 15:01:05 -0700

    Create first draft

---
+++ csv_to_json.rb
@@ -0,0 +1,22 @@
+require 'csv'
+require 'json'
+
+csv = CSV.parse(File.read('Hawaii_Lanai_Molokai.csv', encoding: 'bom|utf-8'), headers: true )
+
+json = {}
+index = 0
+
+csv.each do |row|
+  pastor = row['pastor']
+  church = 
+  json[index] = {
+    "pastor": row['pastor'],
+    "church": row['church'],
+    "island": row['island'],
+    "city": row['city']
+  }
+  index += 1
+end
+
+f = File.open("data.json", "w")
+f.puts(json.to_json)
---
+++ main.js
@@ -0,0 +1,36 @@
+import data from "./data.json" assert { type: 'json' };;
+
+console.log(data);
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Select a random object
+  const randomIndex = Math.floor(Math.random() * Object.keys(data).length);
+
+  console.log(randomIndex);
+  
+  const content = data[randomIndex.toLocaleString()];
+
+  console.log(content);
+
+  // Display content on the page
+  const displayContainer = document.getElementById('container');
+  const contentHTML = `
+    <div class='contentContainer pastor'>
+      <p class='label'>Pastor</p>
+      <p class='content'>${content.pastor}</p>
+    </div>
+    <div class='contentContainer church'>
+      <p class='label'>Church</p>
+      <p class='content'>${content.church}</p>
+    </div>
+    <div class='contentContainer city'>
+      <p class='label'>City</p> 
+      <p class='content'>${content.city}</p>
+    </div>
+    <div class='contentContainer island'>
+      <p class='label'>Island</p>
+      <p class='content'>${content.island}</p>
+    </div>
+  `;
+  displayContainer.innerHTML = contentHTML;
+});
